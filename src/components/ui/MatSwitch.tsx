471676eca928bcea7b658a29428ab2c641dd7443
Amirhosein Tavakoli <ah.t222amir22@gmail.com>
2024-08-17 08:29:53 +0430

    define MatSwitch component

---
+++ src/components/ui/MatSwitch.tsx
@@ -0,0 +1,33 @@
+import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+import {
+    Switch,
+    SwitchProps
+} from "@mui/material";
+import { boolean, define } from "@react-form-builder/core";
+
+const MatSwitchIcon = () => {
+  return <ToggleOffIcon sx={{ color: "tomato" }} />;
+};
+
+type MatSwitchProps = SwitchProps & {
+  onChange?: (value: any) => void;
+};
+
+const MatSwitch = (props: MatSwitchProps) => {
+  const { onChange, ...otherProps } = props;
+  return (
+    <Switch
+      {...otherProps}
+      onChange={(event) => {
+        onChange?.(event.target.value);
+      }}
+    />
+  );
+};
+
+export const matSwitch = define(MatSwitch, "MatSwitch")
+  .name("Switch")
+  .props({
+    checked: boolean.valued.default(true),
+  })
+  .icon(MatSwitchIcon);
