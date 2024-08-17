e709be140d3db7646c7aba4a6ced99cdb30deb75
Amirhosein Tavakoli <ah.t222amir22@gmail.com>
2024-08-17 08:18:48 +0430

    register mui custom slider for builder

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,6 +4,7 @@ import { matCheckbox } from "./MatCheckbox";
 import { matRadioGroup } from "./MatRadioGroup";
 import { matRating } from "./MatRating";
 import { matSelect } from "./MatSelect";
+import { matSlider } from "./MatSlider";
 import { matTextField } from "./MatTextField";
 // import { matGrid } from "./MatGrid";
 // import { tailGrid } from "./TailGrid";
@@ -16,6 +17,7 @@ const muiComponents = [
   matButtonGroup,
   matCheckbox,
   matRating,
+  matSlider,
   //   matGrid,
   //   tailGrid,
 ];
